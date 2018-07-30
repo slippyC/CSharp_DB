b993ed8c1e8e3f6caab45ff21804a1f0309e5be8
Christopher Whitehead <cwhitehead73@gmail.com>
2018-07-30 09:23:54 -0500

    Added Artist/Album Selection

--- Dbase_example/MainWindow.xaml.cs
+++ Dbase_example/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace Dbase_example
     /// Author:     Christopher Whitehead<cwhitehead73@gmail.com>
     /// Purpose:    C# Dbase Example using "Chinook SQLite DB" with different types of SQL Queries
     ///             SQLite backend used as DB for portability and availability(no DB Server Setup)               
-    ///             This DOES NOT incorporate Error Checking
+    ///             This DOES NOT incorporate complete Error Checking
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -44,6 +44,35 @@ namespace Dbase_example
             Close();
         }
 
+        private void xArtists_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {                      
+            int aData = ((sender as DataGrid).CurrentItem as ArtistData).ArtistId;
+            this.mAlbums = this.mLibrary.getAlbums(aData);
+            xAlbums.ItemsSource = this.mAlbums;
+            if(this.mAlbums.Count > 0)
+            {
+                xGridTracks.ItemsSource = this.mLibrary.getTracks(this.mAlbums[0].AlbumId);
+            }
+            else
+            {
+                xGridTracks.ItemsSource = null;
+            }    
 
+        }
+
+        private void xAlbums_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                int aData = ((sender as DataGrid).CurrentItem as AlbumData).AlbumId;
+                xGridTracks.ItemsSource = this.mLibrary.getTracks(aData);
+            }
+            catch(System.NullReferenceException ex)
+            {
+                xGridTracks.ItemsSource = null;
+            }
+           
+            
+        }
     }
 }
